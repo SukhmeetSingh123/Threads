4c4f02fd57a5ad12114a53f91da5b1f891a20f0e
Sukhmeet <sukhmeetsingh1122002@gmail.com>
2023-12-19 23:26:26 +0530

    complete project

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -2,7 +2,7 @@ const page = () => {
   return (
     <section>
     <h1 className="head-text mb-10">Communities</h1>
-    
+      <h2 className="text-light-1">Didn't created the community part </h2>
 </section>
   )
 }
